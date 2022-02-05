46f4c3e958d38cdb90f505b4571172c0ac45279d
Rodrigo Oliveira <rodrigo.silvoliveira@gmail.com>
2022-02-05 17:11:10 -0300

    Initial commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController board;
+    public List<Sprite> sprites = new List<Sprite>();
+    private GameObject[,] pecas;
+    public GameObject peca;
+    private int quantidade_x = 22;
+    private int quantidade_y = 12;
+    //private bool trocar_peca;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        board = GetComponent<GameController>();
+         Board(
+            peca.GetComponent<SpriteRenderer>().bounds.size.x,
+            peca.GetComponent<SpriteRenderer>().bounds.size.y
+        );
+    }
+
+    // Update is called once per frame
+    void Update() { }
+
+    private void  Board(float  x_desloc, float  y_desloc)
+    {
+        pecas = new GameObject[quantidade_x, quantidade_y];
+        float X = -8.15f;
+        float Y = -4.25f;
+        Sprite[] aux = new Sprite[quantidade_y];
+        Sprite aux_sprite = null;
+        for (int x = 0; x < quantidade_x; x++)
+        {
+            for (int y = 0; y < quantidade_y; y++)
+            {
+                GameObject aux_peca = Instantiate(
+                    peca,
+                    new Vector3(X + ( x_desloc * x), Y + ( y_desloc * y), 0),
+                    peca.transform.rotation
+                );
+                pecas[x, y] = aux_peca;
+                aux_peca.transform.parent = transform;
+                List<Sprite> novas_pecas = new List<Sprite>();
+                novas_pecas.AddRange(sprites);
+                novas_pecas.Remove(aux[y]);
+                novas_pecas.Remove(aux_sprite);
+                Sprite newSprite = novas_pecas[Random.Range(0, novas_pecas.Count)];
+                aux_peca.GetComponent<SpriteRenderer>().sprite = newSprite;
+                aux[y] = newSprite;
+                aux_sprite = newSprite;
+            }
+        }
+    }
+
+    private Sprite GeraPeca(int x, int y)
+    {
+        List<Sprite> novas_pecas = new List<Sprite>();
+        novas_pecas.AddRange(sprites);
+        if (x > 0)
+        {
+            novas_pecas.Remove(pecas[x - 1, y].GetComponent<SpriteRenderer>().sprite);
+        }
+        if (x < quantidade_x - 1)
+        {
+            novas_pecas.Remove(pecas[x + 1, y].GetComponent<SpriteRenderer>().sprite);
+        }
+        if (y > 0)
+        {
+            novas_pecas.Remove(pecas[x, y - 1].GetComponent<SpriteRenderer>().sprite);
+        }
+        return novas_pecas[Random.Range(0, novas_pecas.Count)];
+    }
+
+    private IEnumerator GeraPecaVazio(int x, int y)
+    {
+        List<SpriteRenderer> aux = new List<SpriteRenderer>();
+        int vazio = 0;
+        for (int i = y; i < quantidade_y; i++)
+        {
+            SpriteRenderer aux2 = pecas[x, i].GetComponent<SpriteRenderer>();
+            if (aux2.sprite == null)
+            {
+                vazio++;
+            }
+            aux.Add(aux2);
+        }
+        for (int i = 0; i < vazio; i++)
+        {
+            yield return new WaitForSeconds(0f);
+            for (int j = 0; j < aux.Count - 1; j++)
+            {
+                aux[j].sprite = aux[j + 1].sprite;
+                aux[j + 1].sprite = GeraPeca(x, quantidade_y - 1);
+            }
+        }
+    }
+
+    public IEnumerator EhVazio()
+    {
+        for (int x = 0; x < quantidade_x; x++)
+        {
+            for (int y = 0; y < quantidade_y; y++)
+            {
+                if (pecas[x, y].GetComponent<SpriteRenderer>().sprite == null)
+                {
+                    yield return StartCoroutine(GeraPecaVazio(x, y));
+                    break;
+                }
+            }
+        }
+        for (int x = 0; x < quantidade_x; x++)
+        {
+            for (int y = 0; y < quantidade_y; y++)
+            {
+                pecas[x, y].GetComponent<PieceController>().AuxRemove();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PieceController.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceController : MonoBehaviour
+{
+    private static PieceController anterior = null;
+    private SpriteRenderer  sprite_aux;
+    private bool comb3 = false;
+    private bool selecionou = false;
+    private Vector2[] prox_pecas = new Vector2[] {  Vector2.up,
+													Vector2.down,
+													Vector2.left,
+													Vector2.right };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         sprite_aux = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update() { }
+
+    public void Troca(SpriteRenderer sprite)
+    {
+        if ( sprite_aux.sprite == sprite.sprite)
+        {
+            return;
+        }
+        Sprite aux_sprite = sprite.sprite;
+        sprite.sprite =  sprite_aux.sprite;
+         sprite_aux.sprite = aux_sprite;
+    }
+
+    private void Selecionar()
+    {
+        selecionou = true;
+		sprite_aux.color = new Color(.5f, .5f, .5f, 1.0f);
+        anterior = gameObject.GetComponent<PieceController>();
+    }
+
+    private void Deselecionar()
+    {
+        selecionou = false;
+        sprite_aux.color = Color.white;
+        anterior = null;
+    }
+
+    void OnMouseDown()
+    {
+        if (selecionou)
+        {
+            Deselecionar();
+        }
+        else
+        {
+            if (anterior == null)
+            {
+                Selecionar();
+            }
+            else
+            {
+                if (GetProx().Contains(anterior.gameObject))
+                {
+                    Troca(anterior. sprite_aux);
+                    anterior.AuxRemove();
+                    anterior.Deselecionar();
+                    AuxRemove();
+                }
+                else
+                {
+                    anterior.GetComponent<PieceController>().Deselecionar();
+                    Selecionar();
+                }
+            }
+        }
+    }
+
+    private List<GameObject> GetProx()
+    {
+        List<GameObject> aux_vet = new List<GameObject>();
+        for (int i = 0; i < prox_pecas.Length; i++)
+        {
+            aux_vet.Add(AuxGetProx(prox_pecas[i]));
+        }
+        return aux_vet;
+    }
+
+    private GameObject AuxGetProx(Vector2 vet)
+    {
+        RaycastHit2D  rc = Physics2D.Raycast(transform.position, vet);
+        if ( rc.collider != null)
+        {
+            return  rc.collider.gameObject;
+        }
+        return null;
+    }
+
+    public void AuxRemove()
+    {
+        if ( sprite_aux.sprite == null){
+            return;
+		}
+        RemoveComb(new Vector2[2] { Vector2.left, Vector2.right });
+        RemoveComb(new Vector2[2] { Vector2.up, Vector2.down });
+        if (comb3)
+        {
+             sprite_aux.sprite = null;
+            comb3 = false;
+            StopCoroutine(GameController.board.EhVazio());
+            StartCoroutine(GameController.board.EhVazio());
+        }
+    }
+
+    private List<GameObject> AchaComb(Vector2 vet)
+    {
+        List<GameObject> aux_vet = new List<GameObject>();
+        RaycastHit2D  rc = Physics2D.Raycast(transform.position, vet);
+        while (
+             rc.collider != null &&  rc.collider.GetComponent<SpriteRenderer>().sprite ==  sprite_aux.sprite
+        )
+        {
+            aux_vet.Add( rc.collider.gameObject);
+             rc = Physics2D.Raycast( rc.collider.transform.position, vet);
+        }
+        return aux_vet;
+    }
+
+    private void RemoveComb(Vector2[]  vet)
+    {
+        List<GameObject> aux_vet = new List<GameObject>();
+        for (int i = 0; i <  vet.Length; i++)
+        {
+            aux_vet.AddRange(AchaComb( vet[i]));
+        }
+        if (aux_vet.Count >= 2)
+        {
+            for (int i = 0; i < aux_vet.Count; i++)
+            {
+                aux_vet[i].GetComponent<SpriteRenderer>().sprite = null;
+            }
+            comb3 = true;
+        }
+    }
+}
